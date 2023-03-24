8c2b6a59f05ee42780329e44731817335a489e83
ZheShen00 <shenzhe2000@gmail.com>
2023-03-24 11:12:33 -0700

    update .py file

--- modeling.py
+++ modeling.py
@@ -5,7 +5,7 @@
 # 
 # ## ——Visualization and Exploratory data analysis
 
-# In[56]:
+# In[1]:
 
 
 import pandas as pd
@@ -18,7 +18,7 @@ import seaborn as sns
 # ## Checking the dataframe and choosing features
 # First, we import the cleaned dataset. The whole dataset has 12 features as well as over 400,000 movie data.
 
-# In[57]:
+# In[2]:
 
 
 imdb = pd.read_csv('imdb.csv')
@@ -27,7 +27,7 @@ imdb
 
 # In our program, ratings are the most important indicator of a film's popularity. So we can start by looking at the specific data of the ratings.
 
-# In[58]:
+# In[3]:
 
 
 imdb.averageRating.describe()
@@ -41,7 +41,7 @@ imdb.averageRating.describe()
 # 
 # Then, let's explore which factors are strongly correlated with the scores, which will greatly help our machine learning model to make predictions:
 
-# In[59]:
+# In[4]:
 
 
 # Check how many factors our data set have
@@ -52,92 +52,108 @@ imdb.columns.values.tolist()
 # 
 # First is the distribution of average rating
 
-# In[60]:
+# In[5]:
 
 
-#评分的分布情况
-plt.figure(figsize=(12, 6))
-sns.kdeplot(imdb['averageRating'])
-plt.title('The distribution of  average rating', fontsize=20, weight='bold', color='black')
+def dis_averageRate(data):
+    plt.figure(figsize=(12, 6))
+    sns.kdeplot(data['averageRating'])
+    plt.title('The distribution of  average rating', fontsize=20, weight='bold', color='black')
+dis_averageRate(imdb)
 
 
 # ## The relationship of ratings and other element
 # 
 
-# In[61]:
+# In[6]:
 
 
-plt.figure(figsize=(10, 8))
-plt.scatter(imdb['numVotes'], imdb['averageRating'], s=10, c='red')
-plt.title('a', fontsize=16, weight='bold')
-plt.show()
+def dis_RateVotes(data):
+    plt.figure(figsize=(10, 8))
+    plt.scatter(data['numVotes'], data['averageRating'], s=10, c='red')
+    plt.title('The distribution of Rating and number of votes', fontsize=16, weight='bold')
+    plt.show()
+dis_RateVotes(imdb)
 
 
 # Regarding the relationship between the number of votes and the ratings, the scatterplot presents an uncorrelated feature. However, we can observe that a large number of votes occurs around a rating of 8.0. This means that good movies are more likely to receive a large number of ratings. But the number of votes is statistically uncorrelated for ratings.
 
-# In[62]:
+# In[8]:
 
 
-plt.figure(figsize=(8, 8))
-sns.boxplot(x="isAdult", y="averageRating", data=imdb, linewidth=1.5)
-plt.title('The distribution of Rating and Aldut/Not Aldut',fontsize=20, weight='bold')
-plt.show()
+def dis_RateAldut(data):
+    plt.figure(figsize=(8, 8))
+    sns.boxplot(x="isAdult", y="averageRating", data=data, linewidth=1.5)
+    plt.title('The distribution of Rating and Aldut/Not Aldut',fontsize=20, weight='bold')
+    plt.show()
+dis_RateAldut(imdb)
 
 
 # boxplot shows that whether a movie is adult-rated or not makes big difference in terms of rating. But not adult movie got higher mean value, so when we doing the machine learning model, this factor would be considered
 
-# In[63]:
+# In[9]:
 
 
-df = imdb.startYear.value_counts()
-x = [i for i in df.keys()]
-y = []
-for i in range(0,11):
-    y.append(df.values[i])
-plt.figure(figsize=(10, 6))
-plt.title('The distribution of film release year', fontsize=20, weight='bold')
-sns.barplot(x=x, y=y)
+def dis_ReleaseYear(data):
+    df = data.startYear.value_counts()
+    x = [i for i in df.keys()]
+    y = []
+    for i in range(0,11):
+        y.append(df.values[i])
+    plt.figure(figsize=(10, 6))
+    plt.title('The distribution of film release year', fontsize=20, weight='bold')
+    sns.barplot(x=x, y=y)
+dis_ReleaseYear(imdb)
 
 
-# In[64]:
+# In[10]:
 
 
-plt.figure(figsize=(20, 8))
-sns.boxplot(x="startYear", y="averageRating", data=imdb, linewidth=1.5)
-plt.title('The distribution of Rating and release year',fontsize=20, weight='bold')
-plt.show()
+def dis_RateRelease(data):
+    plt.figure(figsize=(20, 8))
+    sns.boxplot(x="startYear", y="averageRating", data=data, linewidth=1.5)
+    plt.title('The distribution of Rating and release year',fontsize=20, weight='bold')
+    plt.show()
+dis_RateRelease(imdb)
 
 
 # We can see that people generally have better ratings for new movies released after 2021. Despite the huge impact of the pandemic on the movie industry in 2020 resulting in a drop in the number of releases, we still have 35,000 data per year(except 2023), so it is still a valid conclusion that the newer the movie, the more likely it is to receive a high rating.
 
-# In[65]:
+# In[17]:
 
 
-df1 = imdb
-df1 = df1.drop(['genres'], axis=1).join(df1['genres'].str.split(',', expand=True).stack().reset_index(level=1, drop=True).rename('genres'))
-df1
+def Split_genres(data):
+    global df1
+    df1 = data
+    df1 = df1.drop(['genres'], axis=1).join(df1['genres'].str.split(',', expand=True).stack().reset_index(level=1, drop=True).rename('genres'))
+    return df1
+Split_genres(imdb)
 
 
-# In[66]:
+# In[18]:
 
 
-df2 = df1.genres.value_counts()
-x = [i for i in df2.keys()]
-y = []
-for i in range(0,28):
-    y.append(df2.values[i])
-plt.figure(figsize=(26, 8))
-plt.title('The distribution of genres', fontsize=30, weight='bold')
-sns.barplot(x=x, y=y)
+def dis_genres(data):
+    df2 = df1.genres.value_counts()
+    x = [i for i in df2.keys()]
+    y = []
+    for i in range(0,28):
+        y.append(df2.values[i])
+    plt.figure(figsize=(26, 8))
+    plt.title('The distribution of genres', fontsize=30, weight='bold')
+    sns.barplot(x=x, y=y)
+dis_genres(df1)
 
 
-# In[67]:
+# In[19]:
 
 
-plt.figure(figsize=(30, 8))
-sns.boxplot(x="genres", y="averageRating", data=df1, linewidth=1.5)
-plt.title('The distribution of Rating and genres',fontsize=20, weight='bold')
-plt.show()
+def dis_RateGenre(data):
+    plt.figure(figsize=(30, 8))
+    sns.boxplot(x="genres", y="averageRating", data=data, linewidth=1.5)
+    plt.title('The distribution of Rating and genres',fontsize=20, weight='bold')
+    plt.show()
+dis_RateGenre(df1)
 
 
 # Here, we can see that Horror, Sci-Fi, Mystery and Western movies are less likely to get high ratings. While Documentary, Drama, Crime, Animation, Musical, History, Biography titles are most likely to get high ratings (except for some genres with too small a sample size).
@@ -146,52 +162,52 @@ plt.show()
 # 
 # Based on the above analysis, we will make movie rating predictions mainly for genres and release years. In the next work, we will use multiple linear regression models, decision tree regression models, random forest regression models, etc. to predict movie ratings.
 
-# In[68]:
+# In[30]:
 
 
-drop_columns = ["Unnamed: 0", "tconst", "nconst", "category", "primaryTitle", "primaryName"]
-df_model = df1.drop(drop_columns, axis=1)
-df_model
+def drop_Column(data):
+    drop_columns = ["Unnamed: 0", "tconst", "nconst", "category", "primaryTitle", "primaryName"]
+    global df_model
+    df_model = data.drop(drop_columns, axis=1)
+    return df_model
+drop_Column(df1)
 
 
-# In[69]:
-
-
-df_model.info()
-
-
-# In[70]:
-
-
-df_model['genres'] = df_model['genres'].replace({r'\N': None})
-df_model['runtimeMinutes'] = df_model['runtimeMinutes'].replace({r'\N': None}).astype(float)
-
-
-# In[71]:
-
-
-df_model
-
-
-# In[72]:
+# In[27]:
 
 
 df_model.info()
 
 
-# In[73]:
+# In[31]:
+
+
+def replace_N(data):
+    data['genres'] = data['genres'].replace({r'\N': None})
+    data['runtimeMinutes'] = data['runtimeMinutes'].replace({r'\N': None}).astype(float)
+    return df_model
+replace_N(df_model)
+
+
+# In[33]:
+
+
+df_model.info()
+
+
+# In[34]:
 
 
 df_model = df_model.dropna()
 
 
-# In[74]:
+# In[35]:
 
 
 df_model = df_model.reset_index(drop=True)
 
 
-# In[75]:
+# In[36]:
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -204,14 +220,14 @@ df_model['genres'] = labelencoder.fit_transform(df_model['genres'])
 df_model.head()
 
 
-# In[76]:
+# In[37]:
 
 
 y = df_model['averageRating']
 x = df_model.drop(['averageRating'], axis = 1)
 
 
-# In[77]:
+# In[38]:
 
 
 from sklearn.model_selection import train_test_split
@@ -219,25 +235,29 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 
-# In[82]:
+# In[41]:
 
 
 from sklearn.ensemble import RandomForestRegressor
 
-regr = RandomForestRegressor()
-regr.fit(X_train, y_train)
-regr.score(X_test, y_test)
+def RFR_model(train_data1,train_data2):
+    regr = RandomForestRegressor()
+    regr.fit(train_data1,train_data2)
+    return regr.score(X_test, y_test)
+RFR_model(X_train,y_train)
 
 
-# In[163]:
+# In[42]:
 
 
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-model = LinearRegression()
-model.fit(X_train, y_train)
-predict_valid = model.predict(X_test)
-score = r2_score(y_test,predict_valid)
-score
+def LR_model(train_data1,train_data2):
+    model = LinearRegression()
+    model.fit(train_data1,train_data2)
+    predict_valid = model.predict(X_test)
+    score = r2_score(y_test,predict_valid)
+    return score
+LR_model(X_train, y_train)
 
